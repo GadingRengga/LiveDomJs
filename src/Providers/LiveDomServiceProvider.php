136231a65da4c3e24cd4a4de208bda55468b7063
gadingsobad <gading.soloabadi@gmail.com>
2025-06-29 22:14:06 +0700

    fix: Correct asset publishing paths in LiveDomServiceProvider for accuracy

--- src/Providers/LiveDomServiceProvider.php
+++ src/Providers/LiveDomServiceProvider.php
@@ -19,8 +19,8 @@ class LiveDomServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            dirname(__DIR__, 3) . '/resources/js' => public_path('vendor/livedomjs'),
-            dirname(__DIR__, 3) . '/resources/views' => resource_path('views/vendor/livedomjs'),
+            dirname(__DIR__, 2) . '/resources/js' => public_path('vendor/livedomjs'),
+            dirname(__DIR__, 2) . '/resources/views' => resource_path('views/vendor/livedomjs'),
         ], 'livedomjs-assets');
     }
 }
